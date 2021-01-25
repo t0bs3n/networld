29a7625c382bf6225b64559b1151562167d56d0f
t0bs3n <77977485+t0bs3n@users.noreply.github.com>
2021-01-25 20:21:28 +0100

    Add files via upload

---
+++ 6rhd1p.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="6rhd1p" tilewidth="32" tileheight="32" tilecount="208" columns="8">
+ <image source="C:/Users/Admin/Downloads/6rhd1p.gif" width="256" height="832"/>
+</tileset>
---
+++ moebel.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="AVymFB" tilewidth="32" tileheight="32" tilecount="184" columns="8">
+ <image source="C:/Users/Admin/Downloads/AVymFB.gif" width="256" height="736"/>
+</tileset>
