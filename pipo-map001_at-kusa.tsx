83a0e5bcebce84eae465836a02484ee4dbafe683
t0bs3n <77977485+t0bs3n@users.noreply.github.com>
2021-01-25 19:21:55 +0000

    Deploying to gh-pages from master @ 29a7625c382bf6225b64559b1151562167d56d0f 🚀

---
+++ pipo-map001_at-kusa.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pipo-map001_at-kusa" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="C:/Users/Admin/Downloads/Pipoya RPG World Tileset 48x48 40x40 32x32/32x32/[A]_type3/pipo-map001_at-kusa.png" width="256" height="192"/>
+</tileset>
